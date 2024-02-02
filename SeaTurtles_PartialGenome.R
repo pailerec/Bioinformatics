17d008e521d1ece713726b6338fa5b076afabccf
pailerec <158227960+pailerec@users.noreply.github.com>
2024-02-01 19:18:01 -0500

    Create SeaTurtles_PartialGenome.R

---
+++ SeaTurtles_PartialGenome.R
@@ -0,0 +1,12 @@
+seq_1 <- readDNAStringSet("~/Documents/GitHub/Bioinformatics/Sea-Turtle-10.fasta")
+seq_2 <- readDNAStringSet("~/Documents/GitHub/Bioinformatics/Sea-Turtle-11.fasta")
+seq_3 <- readDNAStringSet("~/Documents/GitHub/Bioinformatics/Sea-Turtle-12.fasta")
+seq_4 <- readDNAStringSet("~/Documents/GitHub/Bioinformatics/Sea-Turtle-13.fasta")
+seq_5 <- readDNAStringSet("~/Documents/GitHub/Bioinformatics/Sea-Turtle-14.fasta")
+seq_1
+seq_2
+seq_3
+sequences <- c(seq_1, seq_2, seq_3, seq_4, seq_5)
+sequences
+
+
