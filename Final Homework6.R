342369e98760ec8f695c23b58e0d6768afbf82ca
pailerec <158227960+pailerec@users.noreply.github.com>
2024-02-13 20:39:47 -0500

    Homework

---
+++ Final Homework6.R
@@ -0,0 +1,12 @@
+A0A023J8S8
+A2BD37
+A0A6A5DLT6
+A0A068L9A8
+A0A0U1YZZ6
+STACC <- c("A0A023J8S8", "A2BD37", "A0A6A5DLT6", "A0A068L9A8", "A0A0U1YZZ6")
+library(UniprotR)
+library(protti)
+STACC2<- GetProteinGOInfo(STACC)
+PlotGoInfo(STACC2)
+GetPathology_Biotech(STACC2)
+Get.diseases(STACC2)
